75686354ad89e4afc48b64d10579f0c787e8a42a
Hsun-Yi Hsieh <hhsieh@users.noreply.github.com>
2016-11-23 19:27:00 -0500

    Update intersection.py

--- intersection.py
+++ intersection.py
@@ -1,5 +1,8 @@
 ## Use intersection to find out whether two strings share the same substring(s)
 
+##############################
+## First, some basic things ##
+##############################
 # map two strings
 a = map(str, input())
 b = map(str, input())
@@ -13,8 +16,10 @@ if len(d) > 0:
 else:
     print "No"
 
-    
-## We can write a function to accommodate the above
+######################################################    
+## We can write a function to accommodate the above ##
+######################################################
+
 def intersect():
     a = map(str, raw_input())
     b = map(str, raw_input())
