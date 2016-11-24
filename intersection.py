b13ba8e75a8acb9daec2e53be239629939b4faea
Hsun-Yi Hsieh <hhsieh@users.noreply.github.com>
2016-11-23 19:22:28 -0500

    Update intersection.py

--- intersection.py
+++ intersection.py
@@ -5,10 +5,29 @@ a = map(str, input())
 b = map(str, input())
 
 # intersect a and b
-c = set(a).intersection(b)
+d = set(a).intersection(b)
 
 # if the length of c is greater than 0, print "Yes" - a and b share the same substring(s). Otherwise, print "No".
-if len(c) > 0:
+if len(d) > 0:
     print "Yes"
 else:
     print "No"
+
+    
+## We can write a function to accommodate the above
+def intersect():
+    a = map(str, raw_input())
+    b = map(str, raw_input())
+    d = set(a).intersection(b)
+    if len(d) > 0:
+        print "YES"
+    else:
+        print "NO"
+
+## call the function intersct for multiple times
+
+c = int(input())
+
+for _ in range(c):
+    intersect()
+    
