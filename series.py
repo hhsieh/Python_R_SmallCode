0d5ac7e4c37bd45398fb936c348c5a697b16ee57
Hsun-Yi Hsieh <hhsieh@users.noreply.github.com>
2016-11-24 22:49:47 -0500

    Update series.py

--- series.py
+++ series.py
@@ -1,7 +1,7 @@
 ## You have a series of numbers: 3, 4, 5, 7, 9, 12, 15... What will be the Nth number (N >= 1)?
 
-## enter the 
-N = int(input())
+## It is easy to find out the rule of addition in the sequence of numbers is 1, 1, 2, 2, 3, 3, ...
+## We can write a function to reflect the above rule.
 
 def series(N):
     if N % 2 == 0:
